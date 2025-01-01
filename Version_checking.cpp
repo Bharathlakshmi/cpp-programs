9fbb059ab8ebd38713efba8c2ea5aa28a7c76ac4
Bharathlakshmi <bharathlakshmiselvaraj11@gmail.com>
2025-01-01 11:48:12 +0530

    feat : added the question

--- Version_checking.cpp
+++ Version_checking.cpp
@@ -1,3 +1,24 @@
+/*
+A version number is a string that is used to identify the unique state of a software product. A version number looks 
+like a.b.c.d, where a, b, etc are numbers, so the version number is a string in which numbers are separated by dots. 
+These numbers generally represent the hierarchy from major to minor (a is major and d is minor). 
+In this problem, we are given two version numbers. We need to compare them and conclude which one is the 
+latest version number (that is, which version number is greater). 
+
+Example: 
+Input: 
+V1 = “1.0.31”
+V2 = “1.0.27”
+Output:  v1 is latest
+Because V2 < V1
+
+Input: 
+V1 = “1.0.10”
+V2 = “1.0.27”
+Output:  v2 is latest
+Because V1 < V2 
+*/
+
 #include<iostream>
 #include<string.h>
 using namespace std;
